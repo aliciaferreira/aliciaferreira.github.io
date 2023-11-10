6629a26c1869ecd8515f1353d228e90d8160d67f
dana <danamore@live.com.ar>
2023-11-09 21:49:58 -0300

    Re ert "agrego more.html y preferencias.php"
    This reverts commit bc366bc5055e9a994055ad26551460b6cc4c71ca.
    
    Re

--- preferencias.php
+++
@@ -1,7 +0,0 @@
-<?php
-require("more.html");
-
-$pelicula = (isset($_GET['pelicula']) ? $_GET['pelicula'] : "No cargaste tus películas de preferencia");
-
-
-echo "Tu película de preferencia es: " . $pelicula;
