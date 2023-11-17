3f2959e6c7a491a6b4928384e01aa0324d695e26
Mariano Facundo Morbelli <144305761+Morbelli@users.noreply.github.com>
2023-11-16 21:20:13 -0300

    último arreglo

--- morbelli.php
+++ morbelli.php
@@ -1,14 +1,14 @@
 <?php
-require ("preferencias.html");
+require ("morbelli.html");
 
-$usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
+
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-$banda=(isset($_GET['banda'])?$_GET['banda']:"No cargaste una banda preferido");
 
 
-echo "Hola $usuario";
+
+
 echo "Tu color preferido es: $color";
-echo "Tu banda preferida es: $banda";
+
 
 
 
