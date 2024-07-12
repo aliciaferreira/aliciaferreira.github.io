36a75845b37cc8865684d046f6b2ae4d941c1a2f
Bogarin1978 <bogarinjorgeramon7@gmail.com>
2024-07-11 21:41:57 -0300

    resolve all

---
+++ bogarin.php
@@ -0,0 +1,10 @@
+<?php
+
+require("bogarin.html.html");
+
+$usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
+$localidad=(isset($_GET['llcalidad'])?$_GET['localidad']:"No pusiste localidad");
+?><br><br><?php
+echo "Hola $usuario";?><br><?php
+echo "Tu color preferido es: $localidad";?><br><br><?php
+?>
