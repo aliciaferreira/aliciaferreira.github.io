8a1d448365f81fe75845d27229d4c4435cd62d00
juli1097 <lordjuli97@gmail.com>
2024-07-11 21:11:38 -0300

    modific acunia and add acunia.html

--- acunia.php
+++ acunia.php
@@ -1,5 +1,5 @@
 <?php
-require("ferreira.html");
+require("acunia.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
 $color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
