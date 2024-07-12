af65057f9e2cf78c65d11077c947257939381d7b
FernandoALemos <fernando.alberto.lemos@gmail.com>
2024-07-11 21:39:46 -0300

    preferencias: juego de mesa

---
+++ lemos.php
@@ -0,0 +1,9 @@
+<?php
+    require("ferreira.html");
+
+    $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
+    $juego_de_mesa=(isset($_GET['juego_de_mesa'])?$_GET['juego_de_mesa']:"No cargaste un juego de mesa preferido");
+    ?><br><br><?php
+    echo "Hola $usuario";?><br><?php
+    echo "Tu juego de mesa preferido es: $juego_de_mesa";?><br><br><?php
+?>
