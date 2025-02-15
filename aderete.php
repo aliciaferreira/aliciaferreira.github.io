db79f2170acd795e17a38b4ed5943c55d26b9308
Luigii02 <luigii.alderete02@gmail.com>
2025-02-14 21:10:03 -0300

    reenvio de los archivos php y html con nombre aderete

--- aderete.php
+++ aderete.php
@@ -2,10 +2,8 @@
 require("aderete.html");
 
 $usuario=(isset($_GET['usuario'])?$_GET['usuario']:"No cargaste un nombre de usuario");
-$color=(isset($_GET['color'])?$_GET['color']:"No cargaste un color preferido");
-$juego=(isset($_GET['juego'])?$_GET['juego']:"No cargaste un juego preferido");
+$color=(isset($_GET['comida'])?$_GET['comida']:"No cargaste una comida preferida");
 ?><br><br><?php
 echo "Hola $usuario";?><br><?php
-echo "Tu color preferido es: $color";?><br><br><?php
-echo "Tu juego preferido es: $juego";?><br><br><?php
+echo "Tu comida preferido es: $comida";?><br><br><?php
 ?>
